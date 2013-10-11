986ef7044df3b4e425f0a46113fea251ac53ba5c
Lester Sy <lsy@klick.com>
2013-10-11 11:20:57 -0400

    added resizing checks.

--- js/game.js
+++ js/game.js
@@ -1,45 +1,45 @@
 var KEY_LEFT = 37, KEY_RIGHT = 39, KEY_UP = 38, KEY_DOWN = 40, KEY_A = 65, KEY_S = 83, KEY_D = 68, KEY_W = 87, KEY_SPACE = 32;
 
-function gameLoop(){
+function gameLoop() {
     update();
     draw();
 }
 
-function draw(){
+function draw() {
     stage.update();
 }
 
 var prevTime = 0;
 var prevLeft = false, prevRight = false, prevUp = false, prevDown = false;
-function update(){
+function update() {
     var totalTime = createjs.Ticker.getTime();
     var elapsedTime = totalTime - prevTime;
     prevTime = createjs.Ticker.getTime();
 
     updateGamepad(elapsedTime);
-    
-    if (pressedKeys[KEY_UP] || pressedKeys[KEY_W]){
-        moveUp(elapsedTime, 100);
+
+    if (pressedKeys[KEY_UP] || pressedKeys[KEY_W]) {
+        moveUp(elapsedTime, 100, 0);
     }
-    if (pressedKeys[KEY_DOWN] || pressedKeys[KEY_S]){
-        moveDown(elapsedTime, 100);
+    if (pressedKeys[KEY_DOWN] || pressedKeys[KEY_S]) {
+        moveDown(elapsedTime, 100, 0);
     }
-    if (pressedKeys[KEY_LEFT] || pressedKeys[KEY_A]){
-        moveLeft(elapsedTime, 100);
+    if (pressedKeys[KEY_LEFT] || pressedKeys[KEY_A]) {
+        moveLeft(elapsedTime, 100, 0);
     }
-    if (pressedKeys[KEY_RIGHT] || pressedKeys[KEY_D]){
-        moveRight(elapsedTime, 100);
+    if (pressedKeys[KEY_RIGHT] || pressedKeys[KEY_D]) {
+        moveRight(elapsedTime, 100, 0);
         //currval += elapsedTime;
         //if (currval > threshold){
         //    moveRight(elapsedTime, 10000);
         //    currval = 0;
         //}
-    } 
+    }
     //else {
     //    currval = 0;
     //}
-    if (pressedKeys[KEY_SPACE]){
-        
+    if (pressedKeys[KEY_SPACE]) {
+
     }
 }
 
@@ -47,29 +47,74 @@ function updateGamepad(elapsedTime) {
     if (gamepadSupport.available) {
         gamepadSupport.tick();
 
+        var leftX = gamepadSupport.xboxControllerState.STICK_LEFT_X;
+        var leftY = gamepadSupport.xboxControllerState.STICK_LEFT_Y;
+        var rightX = gamepadSupport.xboxControllerState.STICK_RIGHT_X;
+        var rightY = gamepadSupport.xboxControllerState.STICK_RIGHT_Y;
+
         // note to self, set axis threshold to 0.25
-        if (gamepadSupport.xboxControllerState.STICK_LEFT_X < -0.5){
-            moveLeft(elapsedTime, 100);
-        } else if (gamepadSupport.xboxControllerState.STICK_LEFT_X > 0.5) {
-            moveRight(elapsedTime, 100);
+        if (leftX < -0.2) {
+            moveLeft(elapsedTime, 200, 0);
+        } else if (leftX > 0.2) {
+            moveRight(elapsedTime, 200, 0);
         }
-        if (gamepadSupport.xboxControllerState.STICK_LEFT_Y < -0.5){
-            moveUp(elapsedTime, 100);
-        } else if (gamepadSupport.xboxControllerState.STICK_LEFT_Y > 0.5) {
-            moveDown(elapsedTime, 100);
+        if (leftY < -0.2) {
+            moveUp(elapsedTime, 200, 0);
+        } else if (leftY > 0.2) {
+            moveDown(elapsedTime, 200, 0);
+        }
+
+        
+        if (rightX < -0.2) {
+            moveLeft(elapsedTime, 200, 1);
+        } else if (rightX > 0.2) {
+            moveRight(elapsedTime, 200, 1);
+        }
+        if (rightY < -0.2) {
+            moveUp(elapsedTime, 200, 1);
+        } else if (rightY > 0.2) {
+            moveDown(elapsedTime, 200, 1);
         }
     }
 }
 
-function moveUp(elapsed, increment){
-    ballBitmap.y -= increment * elapsed / 1000;
+function moveUp(increment, elapsed, ballId) {
+    switch (ballId) {
+        case 0:
+            ball_blue.y -= increment * elapsed / 1000;
+            break;
+        case 1:
+            ball_red.y -= increment * elapsed / 1000;
+            break;
+    }
 }
-function moveDown(increment, elapsed){
-    ballBitmap.y += increment * elapsed / 1000;
+function moveDown(increment, elapsed, ballId) {
+    switch (ballId) {
+        case 0:
+            ball_blue.y += increment * elapsed / 1000;
+            break;
+        case 1:
+            ball_red.y += increment * elapsed / 1000;
+            break;
+    }
 }
-function moveLeft(increment, elapsed){
-    ballBitmap.x -= increment * elapsed / 1000;
+function moveLeft(increment, elapsed, ballId) {
+    switch (ballId) {
+        case 0:
+            ball_blue.x -= increment * elapsed / 1000;
+            break;
+        case 1:
+            ball_red.x -= increment * elapsed / 1000;
+            break;
+    }
 }
-function moveRight(increment, elapsed){
-    ballBitmap.x += increment * elapsed / 1000;
+function moveRight(increment, elapsed, ballId) {
+    switch (ballId) {
+        case 0:
+            ball_blue.x += increment * elapsed / 1000;
+            break;
+        case 1:
+            ball_red.x += increment * elapsed / 1000;
+            break;
+    }
 }
--- js/gamesetup.js
+++ js/gamesetup.js
@@ -1,22 +1,25 @@
-var canvas, stage, preload, cornflowerBlue;
+var canvas, stage, preload, background;
 var pressedKeys={};
-var width = 1024;
-var height = 768;
 
 
-var ballBitmap;
+
+var ball_blue;
+var ball_red;
 
 function initializeGame() {
     console.log('initializing game.');
     canvas = document.getElementById('main-canvas');
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = standardWidth;
+    canvas.height = standardHeight;
     stage = new createjs.Stage(canvas);
     preload = new createjs.LoadQueue(true);
     preload.installPlugin(createjs.Sound);
     preload.addEventListener('complete', prepareAssets);
     this.document.onkeydown = handleKeyDown;
     this.document.onkeyup = handleKeyUp;
+    $(window).resize(function () {
+        redrawAssets();
+    });
 
     var manifest = [
         { id: 'ballImage', src: 'img/ball.png' }
@@ -27,14 +30,16 @@ function initializeGame() {
 
 function prepareAssets() {
     console.log('preparing assets.')
-    cornflowerBlue = new createjs.Shape();
-    cornflowerBlue.graphics.beginFill('#93CCEA').drawRect(0, 0, width, height);
-    stage.addChild(cornflowerBlue);
+    background = new createjs.Shape();
+    background.graphics.beginFill('#93CCEA').drawRect(0, 0, standardWidth, standardHeight);
+    stage.addChild(background);
 
+    prepareVectors();
     prepareBitmaps();
     prepareAnimations();
 
-    stage.addChild(ballBitmap);
+    stage.addChild(ball_blue);
+    stage.addChild(ball_red);
 
     run();
 }
@@ -45,10 +50,58 @@ function run(){
 }
 
 function prepareBitmaps() {
-    var ballImage = preload.getResult('ballImage');
-    ballBitmap = new createjs.Bitmap(ballImage);
+    //var ballImage = preload.getResult('ballImage');
+    //ballBitmap = new createjs.Bitmap(ballImage);
+}
+function prepareVectors() {
+    ball_blue = new createjs.Shape();
+    ball_blue.graphics.beginFill('#0000FF').drawCircle(0, 0, 10);
+    ball_red = new createjs.Shape();
+    ball_red.graphics.beginFill('#FF0000').drawCircle(0, 0, 10);
 }
 function prepareAnimations() { 
+}
+
+// picking 4: 3 ratios
+var screenSizes = {
+    '640x480' : {  width: 640, height: 480, scale: 0.625 },
+    '800x600' : { width: 800, height: 600, scale: 0.78125 },
+    '1024x768' : { width: 1024, height: 768, scale: 1 },
+    '1280x960' : { width: 1280, height: 960, scale: 1.25 }
+};
+var screenSize = '1024x768';
+var standardWidth = 1024;
+var standardHeight = 768;
+
+function redrawAssets() {
+    console.log('redrawing');
+    var screenWidth = $('body').innerWidth();
+    var screenHeight = $('body').innerHeight();
+
+    var targetScreenSize = '1024x768';
+    if (screenWidth < 800) {
+        targetScreenSize = '640x480';
+    } else if (screenWidth < 1024){
+        targetScreenSize = '800x600';
+    } else if (screenWidth < 1280){
+        targetScreenSize = '1024x768';
+    } else {
+        targetScreenSize = '1280x960';
+    }
+
+    if (screenSize == targetScreenSize)
+        return;
+        
+    screenSize = targetScreenSize;
+    var ss = screenSizes[screenSize];
+    canvas.width = ss.width;
+    canvas.height = ss.height;
+
+    for (childIdx in stage.children) {
+        child = stage.getChildAt(childIdx);
+        child.scaleX = ss.scale;
+        child.scaleY = ss.scale;
+    }
 }
 
 function handleKeyDown(e){
